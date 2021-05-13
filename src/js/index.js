fd4ef6b964e8f8315286f58c0af453ebc0c8a381
Rafia7 <raf.arnob8@gmail.com>
2021-05-13 15:13:44 -0400

    IDK

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
-import { DOMSelectors } from "./DOM";
-import { genres } from "./genre";
+//import { DOMSelectors } from "./DOM";
+//import { genres } from "./genre";
 
 const key = "YOURKEYHERE";
+const query = async function () {
+  try {
+    const response = await fetch(
+      `https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple`
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    alert("Something is wrong");
+  }
+};
