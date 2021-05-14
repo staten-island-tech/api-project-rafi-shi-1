2cc5221febd767fa85607dc6941fd4dcdc1a0b10
Rafia7 <raf.arnob8@gmail.com>
2021-05-13 20:55:18 -0400

    Some progress (barely)

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
-//import { DOMSelectors } from "./DOM";
-//import { genres } from "./genre";
+const DOMSelectors = {
+  grid: document.querySelector(".movie-grid"),
+};
 
 const key = "YOURKEYHERE";
 const query = async function () {
@@ -8,7 +9,38 @@ const query = async function () {
       `https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple`
     );
     const data = await response.json();
-    console.log(data);
+    data.results.forEach((trivia) => {
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `<div class="movie-card">
+      <div class="movie-card-front">
+        <img
+          src="https://image.tmdb.org/t/p/w300/r7vmZjiyZw9rpJMQJdXpjgiCOk9.jpg"
+          alt=""
+          class="poster"
+        />
+      </div>
+      <div class="movie-card-back">
+        <h3 class="movie-card-header">${trivia.question}</h3>
+        <div class="score-box">
+          <p class="user-score">${trivia.correct_answer}</p>
+          <p class="user-score"></p>
+        </div>
+
+        <div class="release-box">
+          <p class="release-date">Released</p>
+          <p class="release-date">2020-06-12</p>
+        </div>
+
+        <div class="movie-genres">
+          <li class="movie-genre">Sci-Fi</li>
+          <li class="movie-genre">Fantasy</li>
+          <li class="movie-genre">Horror</li>
+        </div>
+      </div>
+    </div> `
+      );
+    });
   } catch (error) {
     console.log(error);
     alert("Something is wrong");
