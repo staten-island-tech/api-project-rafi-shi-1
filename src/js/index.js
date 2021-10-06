a8a295f547c401ac6bc0c58ed7d76682784f9fad
Rafia7 <raf.arnob8@gmail.com>
2021-10-06 06:49:59 -0400

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,7 @@
 const DOMSelectors = {
   quote: document.querySelector(".quote-display"),
 };
-const response = await fetch(`https://api.chucknorris.io/jokes/random`);
-response();
+
 const query = async function () {
   try {
     const response = await fetch(`https://api.chucknorris.io/jokes/random`);
@@ -38,7 +37,7 @@ const query = async function () {
       );
     });
 
-    /*(fact) => {
+    (fact) => {
       DOMSelectors.quote.insertAdjacentHTML(
         "beforeend",
         `<div class="test">
@@ -62,10 +61,46 @@ const query = async function () {
   
     </div>`
       );
-    };*/
+    };
   } catch (error) {
     console.log(error);
     alert("Something went wrong");
   }
 };
 query();
+const thirty = async function () {
+  fetch("https://api.chucknorris.io/jokes/random")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .then((data) => {
+      let apiData = [];
+      console.log(apiData);
+      apiData.push(data);
+      data.response.array.forEach((fact) => {
+        DOMSelectors.quote.insertAdjacentHTML(
+          "beforeend",
+          `<div class="test">
+        <h1 class="Header"> Chuck Norris Facts </h1>
+      </div>
+      <div class="container">
+        <h2 class="box-title">
+    
+        </h2>s
+        <div class="numbers">
+        </div>
+        <div class="btn">
+          Random
+    
+    
+        </div>
+        <div class="quote-display" id="quote">
+          Fatin ${fact.value}
+        </div>
+    
+    
+      </div>`
+        );
+      });
+    });
+};
+thirty();
