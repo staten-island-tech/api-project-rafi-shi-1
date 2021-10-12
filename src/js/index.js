2f4fb397ec002d8925660d63ba0f4433b6d0c032
Rafia7 <raf.arnob8@gmail.com>
2021-10-12 14:20:27 -0400

    4

--- src/js/index.js
+++ src/js/index.js
@@ -1,106 +1,40 @@
 const DOMSelectors = {
   quote: document.querySelector(".quote-display"),
+  btn: document.querySelector(".btn"),
 };
-
 const query = async function () {
   try {
     const response = await fetch(`https://api.chucknorris.io/jokes/random`);
     const data = await response.json();
-    console.log(data.response);
+    console.log(data);
 
-    /*const arrayData = [];
-    arrayData.push(data);*/
+    const arrayData = [];
+    arrayData.push(data);
 
-    data.response.array.forEach((fact) => {
+    const displayQuote = function (data) {
+      const fact = data;
       DOMSelectors.quote.insertAdjacentHTML(
         "beforeend",
-        `<div class="test">
-      <h1 class="Header"> Chuck Norris Facts </h1>
-    </div>
-    <div class="container">
-      <h2 class="box-title">
-  
-      </h2>s
-      <div class="numbers">
-      </div>
-      <div class="btn">
-        Random
-  
-  
-      </div>
-      <div class="quote-display" id="quote">
-        Fatin ${fact.value}
-      </div>
-  
-  
-    </div>`
-      );
-    });
-
-    (fact) => {
-      DOMSelectors.quote.insertAdjacentHTML(
-        "beforeend",
-        `<div class="test">
-      <h1 class="Header"> Chuck Norris Facts </h1>
-    </div>
-    <div class="container">
-      <h2 class="box-title">
-  
-      </h2>s
-      <div class="numbers">
-      </div>
-      <div class="btn">
-        Random
-  
-  
-      </div>
-      <div class="quote-display" id="quote">
-        Fatin ${fact.value}
-      </div>
-  
-  
-    </div>`
+        `
+    
+    
+        </div>
+        <div id="quote">
+        ${data.value}
+        </div>
+    
+    
+      </div>`
       );
     };
+    displayQuote(data);
   } catch (error) {
     console.log(error);
     alert("Something went wrong");
   }
 };
 query();
-const thirty = async function () {
-  fetch("https://api.chucknorris.io/jokes/random")
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .then((data) => {
-      let apiData = [];
-      console.log(apiData);
-      apiData.push(data);
-      data.response.array.forEach((fact) => {
-        DOMSelectors.quote.insertAdjacentHTML(
-          "beforeend",
-          `<div class="test">
-        <h1 class="Header"> Chuck Norris Facts </h1>
-      </div>
-      <div class="container">
-        <h2 class="box-title">
-    
-        </h2>s
-        <div class="numbers">
-        </div>
-        <div class="btn">
-          Random
-    
-    
-        </div>
-        <div class="quote-display" id="quote">
-          Fatin ${fact.value}
-        </div>
-    
-    
-      </div>`
-        );
-      });
-    });
-};
-thirty();
+
+DOMSelectors.btn.addEventListener("click", () => {
+  window.location.reload(true);
+});
